f12b042feb10495138d0c09021beba05a0b8a924
Daniel Fontana <dfont110@gmail.com>
2020-03-14 16:31:40 -0500

    03 bacteria-time

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+const bacteriaTime = (curr, target) => {
+  if (curr > target) return 'targetNum must be larger than currentNum';
+  let numMins = 0;
+
+  while (curr < target) {
+    curr *= 2;
+    numMins += 20;
+  }
+
+  return numMins;
+};

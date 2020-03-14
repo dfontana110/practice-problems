0d65526e23c913dd97abd1374488f86ee71c9715
Daniel Fontana <dfont110@gmail.com>
2020-03-14 17:01:21 -0500

    07 most-vowels

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+const isVowel = char => 'aeiouAEIOU'.includes(char);
+
+const mostVowels = str => {
+  let currWord = '';
+  let currCount = 0;
+
+  let maxWord = '';
+  let maxCount = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ' && i !== str.length - 1) {
+      currWord += str[i];
+      if (isVowel(str[i])) currCount += 1;
+    } else {
+      if (currCount > maxCount) {
+        maxCount = currCount;
+        maxWord = currWord;
+      }
+      currWord = '';
+      currCount = 0;
+    }
+  }
+
+  return maxWord;
+};

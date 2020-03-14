f53d73b9a3460cbce2993b3649ef1343d025f350
Daniel Fontana <dfont110@gmail.com>
2020-03-14 16:50:37 -0500

    06 my-index-of

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+const myIndexOf = (str, searchVal, startIdx = 0) => {
+  for (let i = startIdx; i <= str.length - searchVal.length; i++) {
+    let subStr = str.slice(i, i + searchVal.length);
+    if (subStr === searchVal) return i;
+  }
+  return -1;
+};

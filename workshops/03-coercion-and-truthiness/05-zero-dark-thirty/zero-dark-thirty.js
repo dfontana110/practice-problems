5e1c8bcef2c6b94d3516ee4722f5ac92343a5de6
Daniel Fontana <dfont110@gmail.com>
2020-03-14 17:23:22 -0500

    05 zero-dark-thirty

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+const zeroDarkThirty = num => {
+  if (num === 0) return NaN;
+  let numString = String(num);
+  let noZeroes = '';
+  for (let i = 0; i < numString.length; i++) {
+    let char = numString[i];
+    if (char !== '0') noZeroes += char;
+  }
+
+  return Number(noZeroes);
+};

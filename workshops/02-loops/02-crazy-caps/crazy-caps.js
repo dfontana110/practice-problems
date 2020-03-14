8f26bbf9393f40270c32028d02f7d7da7a294664
Daniel Fontana <dfont110@gmail.com>
2020-03-14 16:25:21 -0500

    02 crazy-caps

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+const crazyCaps = str => {
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    i % 2 ? (newStr += str[i].toUpperCase()) : (newStr += str[i]);
+  }
+
+  return newStr;
+};

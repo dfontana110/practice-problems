acd9995f0bf2d427b2e708b296e78dc28a7949e8
Daniel Fontana <dfont110@gmail.com>
2020-03-14 17:16:19 -0500

    04 only-one

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+const onlyOne = (val1, val2, val3) => {
+  if (val1 && !val2 && !val3) return true;
+  if (val2 && !val1 && !val3) return true;
+  if (val3 && !val1 && !val2) return true;
+
+  return false;
+};

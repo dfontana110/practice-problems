ae95b70d0f4f28caff7aad28e9f712fad8efd429
Daniel Fontana <dfont110@gmail.com>
2020-03-14 16:43:00 -0500

    05 my-slice

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+const mySlice = (orgStr, startIdx = 0, endIdx = orgStr.length) => {
+  let slicedStr = '';
+
+  for (let i = startIdx; i < endIdx; i++) {
+    slicedStr += orgStr[i];
+  }
+  return slicedStr;
+};

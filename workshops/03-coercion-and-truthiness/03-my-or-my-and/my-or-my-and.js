e186f661ef8a7c57125c2b7d987b052cbb631f3b
Daniel Fontana <dfont110@gmail.com>
2020-03-14 17:13:30 -0500

    03 my-or-my-add

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+const myOr = (val1, val2, val3) => {
+  if (val1) return val1;
+  if (val2) return val2;
+  if (val3) return val3;
+
+  return val3;
+};
+
+const myAnd = (val1, val2, val3) => {
+  if (!val1) return val1;
+  if (!val2) return val2;
+  if (!val3) return val3;
+
+  return val3;
+};

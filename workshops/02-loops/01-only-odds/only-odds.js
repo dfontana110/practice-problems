578be5f9af4c97ccbee82bd2b1fa20b5483c67eb
Daniel Fontana <dfont110@gmail.com>
2020-03-14 16:17:03 -0500

    01 only-odds

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+const onlyOdds = num => {
+  if (num < 1) return 0;
+  let sum = 0;
+  for (let i = num; i >= 1; i--) {
+    if (i % 2 === 1) sum += i;
+  }
+  return sum;
+};

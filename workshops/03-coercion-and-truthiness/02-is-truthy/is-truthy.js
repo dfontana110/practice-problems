4592e0eb15fac08ef78388fe94fc3619d417e0f1
Daniel Fontana <dfont110@gmail.com>
2020-03-14 17:08:45 -0500

    02 is-truthy

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+const isTruthy = val => {
+  if (val === false) return 'The boolean value false is falsey';
+  if (val === null) return 'The null value is falsey';
+  if (val === undefined) return 'undefined is falsey';
+  if (val === 0) return 'The number 0 is falsey (the only falsey number)';
+  if (val === '') return 'The empty string is falsey (the only falsey string)';
+
+  return true;
+};

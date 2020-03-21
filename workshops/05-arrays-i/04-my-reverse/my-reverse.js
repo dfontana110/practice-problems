97b4d7ede5a7c468c575e7ab42d93975ef4bd71b
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:21:52 -0500

    04 my-reverse

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+const myReverse = arr => {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - i - 1];
+    arr[arr.length - i - 1] = temp;
+  }
+  return arr;
+};

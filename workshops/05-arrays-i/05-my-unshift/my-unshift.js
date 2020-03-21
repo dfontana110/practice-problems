a105f7d636dbc7d6a4555b4dbd45fb5f3f530fcd
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:25:49 -0500

    05 my-unshift

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const myUnshift = (arr, val) => {
+  const newArr = [val, ...arr];
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   newArr.push(arr[i]);
+  // }
+
+  return newArr;
+};

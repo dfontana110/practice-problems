b598d29614117003b60dd4b8d10bd6dfe51b15e1
Daniel Fontana <dfont110@gmail.com>
2020-03-21 15:03:01 -0500

    05 remove-columns

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const removeColumns = (grid, numCols) => {
+  return grid.map(col => {
+    let count = 0;
+    while (count < numCols) {
+      col.pop();
+      count++;
+    }
+    return col;
+  });
+};

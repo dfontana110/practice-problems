fcc19c83733259828be84882fdb8ec2ea9e03ba2
Daniel Fontana <dfont110@gmail.com>
2020-03-21 15:11:13 -0500

    06 my-join

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+const myJoin = (arr, sep = ',') => {
+  let joined = '';
+
+  arr.forEach((elem, index) => {
+    if (index !== 0) {
+      joined += sep;
+    }
+    if (elem) joined += elem;
+  });
+
+  return joined;
+};

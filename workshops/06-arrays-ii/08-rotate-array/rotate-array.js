9e9e020f4a7f049f742118ef312859e0ee46c50f
Daniel Fontana <dfont110@gmail.com>
2020-03-21 15:28:12 -0500

    08 rotate-array

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const rotateArray = (arr, num) => {
+  const direction = num >= 1 ? 'right' : 'left';
+
+  const rotated = arr.slice();
+
+  for (let i = 0; i < Math.abs(num); i++) {
+    direction === 'right'
+      ? rotated.unshift(rotated.pop())
+      : rotated.push(rotated.shift());
+  }
+
+  return rotated;
+};

dd857e6b4adad06fa20ef6e859059b7b46be3790
Daniel Fontana <dfont110@gmail.com>
2020-03-21 15:17:34 -0500

    07 my-slice

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+const mySlice = (arr, start = 0, end = arr.length) => {
+  if (start < 0) start = arr.length + start;
+  if (end < 0) end = arr.length + end;
+  let sliced = [];
+
+  for (let i = start; i < end; i++) {
+    let elem = arr[i];
+    sliced.push(elem);
+  }
+
+  return sliced;
+};

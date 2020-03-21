0701c1fa4dc2be7c0704ce88e3bda7140de07efa
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:10:26 -0500

    01 odd-couple

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+const oddCouple = arr => {
+  const odds = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2) odds.push(arr[i]);
+    if (odds.length === 2) return odds;
+  }
+
+  return odds;
+};

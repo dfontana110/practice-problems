9912026e7a3f88e4a211e7863a2cc6e317b88b27
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:16:29 -0500

    03 my-last-index-of

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+
+const myLastIndexOf = (arr, searchVal, startIdx = arr.length - 1) => {
+  for (let i = startIdx; i >= 0; i--) {
+    if (arr[i] === searchVal) return i;
+  }
+  return -1;
+};

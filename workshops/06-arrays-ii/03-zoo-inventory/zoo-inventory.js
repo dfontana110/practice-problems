eb30ab3038f2b27b12dda943e292ac3216b0c323
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:55:13 -0500

    03 zoo-inventory

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,7 @@
 // YOUR CODE BELOW
+const zooInventory = zooArr => {
+  return zooArr.map(animalArr => {
+    animalArr = `${animalArr[0]} the ${animalArr[1][0]} is ${animalArr[1][1]}.`;
+    return animalArr;
+  });
+};

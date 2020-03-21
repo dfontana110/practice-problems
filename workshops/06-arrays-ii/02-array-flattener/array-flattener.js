0256495e6d9e1b4e80a414d435ae678c2b361e8a
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:47:09 -0500

    02 array-flattener

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+
+const arrayFlattener = arr => {
+  const flatArr = [];
+
+  arr.forEach(val => {
+    if (Array.isArray(val)) {
+      flatArr.push(...arrayFlattener(val));
+    } else {
+      flatArr.push(val);
+    }
+  });
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   let elem = arr[i];
+  //   if (Array.isArray(elem)) {
+  //     for (let j = 0; j < elem.length; j++) {
+  //       flatArr.push(elem[j]);
+  //     }
+  //   } else {
+  //     flatArr.push(elem);
+  //   }
+  // }
+
+  return flatArr;
+};

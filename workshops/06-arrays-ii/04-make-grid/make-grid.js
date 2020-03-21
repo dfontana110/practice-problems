fc24346c113d18e045be8ad0df4927d75cc074cb
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:58:52 -0500

    04 make-grid

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+const makeGrid = (numCols, numRows) => {
+  const grid = [];
+
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
+    for (let j = 1; j <= numCols; j++) {
+      row.push(j);
+    }
+    grid.push(row);
+  }
+
+  return grid;
+};

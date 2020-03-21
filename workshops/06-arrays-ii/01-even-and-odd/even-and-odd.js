0c8744c6fcda23ac1ca01680e5fe55b2cbb7a47e
Daniel Fontana <dfont110@gmail.com>
2020-03-21 14:35:52 -0500

    01 even-and-odd

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const evenAndOdd = arr => {
+  const evens = [];
+  const odds = [];
+
+  arr.forEach(num => {
+    num % 2 ? odds.push(num) : evens.push(num);
+  });
+  return [evens, odds];
+};
